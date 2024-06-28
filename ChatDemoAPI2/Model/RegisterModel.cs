527ea190b2737cf319020fb9e1a99cfd0aa188dd
Durgesh Marathe <dsmofficial2000@gmail.com>
2024-06-29 01:28:34 +0530

    add table users for register

---
+++ ChatDemoAPI2/Model/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatDemoAPI2.Model
+{
+    public class RegisterModel
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
--- ChatDemoAPI2/Program.cs
+++ ChatDemoAPI2/Program.cs
@@ -1,6 +1,12 @@
+using ChatDemoAPI2.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
